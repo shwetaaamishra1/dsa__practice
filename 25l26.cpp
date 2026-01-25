fad621595154199197febc5fd3241102664aadf4
shwetaaamishra1 <shwetaaamishra1@gmail.com>
2026-01-26 00:17:00 +0530

    Add files via upload

---
+++ 25l26.cpp
@@ -0,0 +1,78 @@
+// its 25 january
+// // code 1
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n=10,row,col;
+//     for(row=1;row<=4;row++){
+//         for(col=1;col<=5;col++){
+
+//             cout<<n<<" ";//prints n and after that a space
+//         }
+//         cout<<endl;// after ever row goes n the next line
+//     }
+// }
+//code 2
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=5,row,col;
+//     for ( row = 1; row<=n; row++)
+//     {
+//         for(col=1;col<=5;col++)
+//         {
+//             cout<<row<<" ";
+//         }
+//     cout<<endl;    
+//     }
+    
+// }
+
+//code 3
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=5,row,col;
+//     for ( row = 1; row<=n; row++)
+//     {
+//         for(col=1;col<=5;col++)
+//         {
+//             cout<<col<<" ";
+//         }
+//     cout<<endl;    
+//     }
+    
+// }
+//code4
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=5,row,col;
+//     for ( row = 1; row<=5; row++)
+//     {
+//         for(col=n;col>=1;col--)
+//         {
+//             cout<<col<<" ";
+//         }
+//     cout<<endl;    
+//     }
+    
+// }
+//code 5
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=5,row,col;
+//     for ( row = 1; row<=n; row++)
+//     {
+//         for(col=1;col<=5;col++)
+//         {
+//             cout<<row*row<<"  ";
+//         }
+//     cout<<endl;    
+//     }
+    
+// }
